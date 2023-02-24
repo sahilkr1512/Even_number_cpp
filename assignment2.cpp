e6de691508852ac981b72b22c5932ceda5909ca5
Sahil Kumar <126187702+sahilkr1512@users.noreply.github.com>
2023-02-24 21:21:30 +0530

    Add files via upload

---
+++ assignment2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int number;
+    cout<<"enter an integer: ";
+    cin>>number;
+    for ( int i =0;i<=number;i++)
+    if (i%2==0)
+    cout<<i<<endl;
+    
+
+
+
+    
+    return 0;
+}
